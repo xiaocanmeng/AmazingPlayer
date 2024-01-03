5824bfa9dc9a16e3a5aefb1e64c40d7aac0aa306
xiaocanmeng <2585609923@qq.com>
2024-01-03 09:53:42 +0800

    version 0.10

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
-// main.cpp
-
-#include "MainWindow.h"
-
-int main() {
-    // 创建MainWindow对象
-    MainWindow mainWindow;
-
-    // 调用showWindow函数
-    mainWindow.showWindow();
-
-    return 0;
+// #include "basic-tutorial.h"
+#include "MediaPipeline.h"
+#include "log.h"
+int main (int argc, char *argv[])
+{
+  InFunLOG();
+#if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
+  return gst_macos_main (tutorial_main, argc, argv, NULL);
+#else
+  // tutorial_main_1(argc,argv);
+  // int32_t ret = tutorial_main_3(argc,argv);
+  
+  Sample sample {};
+  int32_t ret = sample.tutorial_main_3(argc,argv);
+ 
+  printf("ret = %d\n",ret);
+  return ret;
+#endif
 }
